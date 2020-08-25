0a11047878b11b1c31e1d75e90589803170de2bb
Vineeth Hanumanth <39301309+hvinett@users.noreply.github.com>
2020-08-25 19:49:38 +0530

    Update createKeyVaultKey.ps1

--- createKeyVaultKey.ps1
+++ createKeyVaultKey.ps1
@@ -1,3 +1,5 @@
+param([string] $keyVaultName)
+param([string] $keyName)
 az keyvault key create --vault-name $keyVaultName -n $keyName --protection software
 $keyVaultKeyUrl=$(az keyvault key show --vault-name $keyVaultName --name $keyName --query [key.kid] -o tsv)
 Write-Output $keyVaultKeyUrl
